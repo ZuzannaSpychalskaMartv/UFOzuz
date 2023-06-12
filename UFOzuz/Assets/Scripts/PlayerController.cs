b2542a334c40fb5a73e3a9982bb35ea1d537fe64
Zuzanna <zuzannaspychalska20@gmail.com>
2023-06-13 01:50:06 +0200

    Added PickUp script

--- UFOzuz/Assets/Scripts/PlayerController.cs
+++ UFOzuz/Assets/Scripts/PlayerController.cs
@@ -6,14 +6,13 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb2d;
 
-    // Start is called before the first frame update
+
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    // Tick 
+
 
     private void FixedUpdate()
     {
@@ -23,8 +22,12 @@ public class PlayerController : MonoBehaviour
         rb2d.AddForce(movement * 15);
     }
 
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("PickUp"))
+        {
+            Destroy(collision.gameObject); 
+        }
     }
+
 }
